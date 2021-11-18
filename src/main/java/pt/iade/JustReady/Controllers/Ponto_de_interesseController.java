9e8145a86bd3b015fc9091ce955ae95d48a2db27
joaobncarvalho <joaobernardocarvalho02@gmail.com>
2021-11-18 09:03:52 +0000

    commit and push

--- src/main/java/pt/iade/JustReady/Controllers/Ponto_de_interesseController.java
+++ src/main/java/pt/iade/JustReady/Controllers/Ponto_de_interesseController.java
@@ -21,41 +21,41 @@ import pt.iade.JustReady.models.Repositories.Ponto_de_interesseRepository;
 
 
 @RestController
-@RequestMapping(path = "/api/pontos_de_interesse")
+@RequestMapping(path = "/api/ponto_de_interesse")
 public class Ponto_de_interesseController {
     private Logger logger = LoggerFactory.getLogger(Ponto_de_interesseController.class);
     @Autowired
     private Ponto_de_interesseRepository pontodeinteresseRepository;
 
     @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Iterable<Ponto_de_interesse> getPontos_de_interesse() {
+    public Iterable<Ponto_de_interesse> getPonto_de_interesse() {
         logger.info("Sending all PI ");
         return pontodeinteresseRepository.findAll();
     }
 
     @GetMapping(path = "/{id:[0-9]+}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Ponto_de_interesse getPontos_de_interesse(@PathVariable int id) {
+    public Ponto_de_interesse getPonto_de_interesse(@PathVariable int id) {
         logger.info("Sending  with id " + id);
-        Optional<Ponto_de_interesse> _pontosdeinteresse = pontodeinteresseRepository.findById(id);
-        if (!_pontosdeinteresse.isPresent())
-            throw new NotFoundException("" + id, "Pontos_de_interesse", "id");
+        Optional<Ponto_de_interesse> _pontodeinteresse = pontodeinteresseRepository.findById(id);
+        if (!_pontodeinteresse.isPresent())
+            throw new NotFoundException("" + id, "Ponto_de_interesse", "id");
         else
-            return _pontosdeinteresse.get();
+            return _pontodeinteresse.get();
     }
 
     @PostMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Ponto_de_interesse savePontos_de_interesse(@RequestBody Ponto_de_interesse pontos_de_interesse) {
-        Ponto_de_interesse savedPontos_de_interesse = pontodeinteresseRepository.save(pontos_de_interesse);
-        logger.info("Saving pontosdeinteresse with id " + savedPontos_de_interesse.getPi_id());
-        return savedPontos_de_interesse;
+    public Ponto_de_interesse savePonto_de_interesse(@RequestBody Ponto_de_interesse ponto_de_interesse) {
+        Ponto_de_interesse savedPonto_de_interesse = pontodeinteresseRepository.save(ponto_de_interesse);
+        logger.info("Saving pontodeinteresse with id " + savedPonto_de_interesse.getPi_id());
+        return savedPonto_de_interesse;
     }
 
     @DeleteMapping(path = "/{id:[0-9]+}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Response deletePontos_de_interesse(@PathVariable int id) {
-        logger.info("Deleting pontos_de_interesses with id " + id);
+    public Response deletePonto_de_interesse(@PathVariable int id) {
+        logger.info("Deleting ponto_de_interesse with id " + id);
         // No verification to see if it exists
         pontodeinteresseRepository.deleteById(id);
-        return new Response("Deleted pontosdeinteresse with id " + id, null);
+        return new Response("Deleted pontodeinteresse with id " + id, null);
     }
     
 }
